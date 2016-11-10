f79d3be7d4bea9365eca16bf1617aec81b1d3707
santiiisfc <santiagofrancoguerro@gmail.com>
2016-11-10 12:16:22 +0100

    graficas para que el usuario vea sus pedidos, cambios en las dimensiones

--- pdf_usuario.php
+++ pdf_usuario.php
@@ -8,14 +8,14 @@ include_once("./db_configuration.php");
 
 $pdf = new FPDF();
 $pdf->AddPage();
-$pdf->SetFont('courier', '', 10);
-$pdf->Cell(4, 10, '', 0);
+$pdf->SetFont('Arial', '', 10);
+$pdf->Cell(5, 10, '', 0);
 $pdf->Image('libros1.jpg' , 10 ,9.5, 12 , 12,'jpg');
-$pdf->Cell(10,8,'',0);
+$pdf->Cell(11,8,'',0);
 $pdf->Cell(150, 10, 'Libreria Santiago', 0);
-$pdf->SetFont('courier', '', 9);
-$pdf->Cell(50, 10, 'Fecha: '.date('d-m-Y').'', 0);
-$pdf->Ln(18);
+$pdf->SetFont('Arial', '', 9);
+$pdf->Cell(47, 10, 'Fecha: '.date('d-m-Y').'', 0);
+$pdf->Ln(17);
 $pdf->SetFont('Arial', 'B', 14);
 $pdf->Cell(70, 8, '', 0);
 $pdf->Cell(80, 8, 'TODOS LOS PEDIDOS REGISTRADOS', 0);
@@ -30,8 +30,8 @@ $pdf->Cell(25, 8, 'cantidad', 1,0,"C");
 $pdf->Cell(25, 8, 'precio', 1,0,"C");
 $pdf->Ln(8);
 $pdf->SetFont('courier', '', 8);
+//conexion y consulta
 $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
-//sql
 $consulta = ("SELECT * FROM `pedidos` JOIN detallespedido on pedidos.idpedido=detallespedido.idpedido where codigou = ".$_SESSION['id'].";");
 $result = $connection->query($consulta);
 $totalli = 0;
