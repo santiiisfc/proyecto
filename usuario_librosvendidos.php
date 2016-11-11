c023cb4d0a3cba7a41bb3dcdaadde49dc27b8fd3
santiiisfc <santiagofrancoguerro@gmail.com>
2016-11-11 19:05:23 +0100

    con include a temas

--- usuario_librosvendidos.php
+++ usuario_librosvendidos.php
@@ -1,7 +1,12 @@
 <?php
 include_once ("cabecerausu2.php");
 include_once("./db_configuration.php");
-include_once("tema_index.php");
+
+ if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+		include_once("tema_index.php");
+    } 
   $contenido = "";
  $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
   if ($connection->connect_errno) {
