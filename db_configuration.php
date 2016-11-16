318587c853a8d0f6c7eb32b596c587267df6d420
santiiisfc <santiagofrancoguerro@gmail.com>
2016-11-16 20:38:54 +0100

    Update db_configuration.php

--- db_configuration.php
+++ db_configuration.php
@@ -5,11 +5,11 @@
     $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
     $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
     $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
-    $db_name="libreria"; //Openshift db name
+    $db_name=$database; //Openshift db name
   } else {
-    $db_user="root"; //my db user
-    $db_host="localhost"; //my db host
-    $db_password=""; //my db password
-    $db_name="libreria"; //my db name
+    $db_user=$username; //my db user
+    $db_host=$localhost; //my db host
+    $db_password=$password; //my db password ORIGINAL
+    $db_name=$database; //my db name
   }
 ?>
