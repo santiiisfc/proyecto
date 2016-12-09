66b4818542eb55d4eb4a374ebdb29cb3f6734dfc
santiiisfc <santiagofrancoguerro@gmail.com>
2016-12-09 18:19:14 +0100

    Update poesiausuario.php

--- poesiausuario.php
+++ poesiausuario.php
@@ -1,13 +1,8 @@
-<?php
+ <?php
+session_start();
 include_once ("cabecerausu2.php");
 include_once("./db_configuration.php");
- if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-		include_once("tema_index.php");
-    } 
-
-
+ include_once("tema_index.php");
 ?>
   <html>
   	<head>
