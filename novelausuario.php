4fc65b16e6eb871b97ae22f334eceb111b5af993
santiiisfc <santiagofrancoguerro@gmail.com>
2016-12-09 18:06:31 +0100

    Update novelausuario.php

--- novelausuario.php
+++ novelausuario.php
@@ -1,13 +1,6 @@
  <?php
 include_once ("cabecerausu2.php");
 include_once("./db_configuration.php");
- if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-		include_once("tema_index.php");
-    } 
-
-
 
 ?>
   <html>
