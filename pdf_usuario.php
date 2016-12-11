a4718d3625a11a4557acdd3345d0e6fceab3e4df
santiiisfc <santiagofrancoguerro@gmail.com>
2016-12-11 18:46:35 +0100

    ajustes en el PDF usuario

--- pdf_usuario.php
+++ pdf_usuario.php
@@ -34,8 +34,6 @@ $pdf->SetFont('courier', '', 8);
 $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
 $consulta = ("SELECT * FROM `pedidos` JOIN detallespedido on pedidos.idpedido=detallespedido.idpedido where codigou = ".$_SESSION['id'].";");
 $result = $connection->query($consulta);
-$totalli = 0;
-$total = 0;
 while($fila = $result->fetch_object()){
 	$pdf->Cell(25, 8,$fila->idpedido, 1,0,"C");
 	$pdf->Cell(25, 8,$fila->fecha,1,0,"C");
